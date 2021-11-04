8804f0b3d94dc1751bd99f9ca434beb586ecf06f
Torsten Wylegala <torsten.wylegala@volkswagen.de>
2021-11-04 19:59:37 +0100

    Refactored and added setup.py

---
+++ README.md
@@ -0,0 +1,8 @@
+# Connected Car Simulator
+
+## Setup environment
+
+    python -m venv .venv
+    source venv/Scripts/activate
+
+    
---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name = "connected_car_simulation",
+    version = "0.0.1",
+    author = "Torsten Wylegala",
+    author_email = "mail@twyleg.de",
+    description = ("Simulation to experiment with connected car features"),
+    license = "GPL 3.0",
+    keywords = "connected car simulation",
+    url = "https://github.com/twyleg/connected_car_simulation",
+    packages=find_packages(),
+    long_description=read('README.md'),
+    install_requires=[
+        'websockets',
+        'aiohttp'
+    ]
+)
